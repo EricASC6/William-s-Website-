f75f9a888bb806c58918ca75551cb86a271233f8
Eric Chen <ericchen9025@gmail.com>
2019-09-08 20:29:44 -0400

    need to work on slide down nav

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const burger = document.querySelector(".burger");
 // Get the nav-links class
 const navLinks = document.querySelector(".nav-links");
 
+// Get the active class
+const active = document.querySelector(".active");
+
 // Add event listener
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
-
-console.log(navLinks.offsetHeight);
