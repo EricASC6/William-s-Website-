d8e670d34ec229339d1ae9ddbaf6a7219b638045
Eric Chen <ericchen9025@gmail.com>
2019-09-11 21:41:20 -0400

    fixes

--- scrollDownNav.js
+++ scrollDownNav.js
@@ -6,8 +6,10 @@ console.log(nav);
 
 // Add event listener to the window object
 window.addEventListener("scroll", () => {
-  if (document.documentElement.scrollTop > 250) {
-    console.log(true);
+  if (
+    document.documentElement.scrollTop > 250 ||
+    document.body.scrollTop > 250
+  ) {
     nav.classList.add("scroll-down");
   } else {
     nav.classList.remove("scroll-down");
